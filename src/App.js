2d3bfd95d61fc2b21c35178749eb07dce3da0177
ParthKasat <parthkasat@gmail.com>
2021-02-28 15:23:10 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div
+        style={{
+          border: "1px solid black",
+          borderBlockColor: "red",
+          borderRadius: "15px",
+          width: "fit-content",
+          padding: "20px",
+          background: "beige"
+        }}
+      >
+        <h1>Parth Kasat</h1>
+        <h2>Sleeper</h2>
+        <h3>Email</h3>
+        <h4>11111111</h4>
+      </div>
+    </div>
+  );
+}
